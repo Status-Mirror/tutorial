f45a40bd7e88f9a2b089514f300ebec82a94133e
Stuart Morris <sjm630@york.ac.uk>
2018-04-11 10:33:13 +0100

    Initial commit

---
+++ hello_git.cpp
@@ -0,0 +1,12 @@
+//hello_git.cpp
+//A clever spoof on the hello world formula, as this will be my first time
+//actually trying to do git properly
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Hello git!" << endl;
+
+  return 0;
+}
